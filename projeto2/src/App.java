2caf12d4aa40f537bf422ecb12f3b3d177e587e8
brunasantos114 <santos.brust@gmail.com>
2022-08-16 16:15:48 -0300

    Dados

--- projeto2/src/App.java
+++ projeto2/src/App.java
@@ -1,6 +1,10 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Teste commit");
-        System.out.println("Bruna Carolina Oliveira Santos");
-    }
+
+
+
+
+
+public class TipoDados {
+
+   
+
 }
---
+++ projeto2/src/dados.java
@@ -0,0 +1,21 @@
+
+public class dados {
+    
+    public static void main(String[] args) {
+        
+            byte idade = 123;
+            short ano = 2021;
+            int cep = 21070333;
+            long cpf = 98563411456L;
+            float pi = 3.14F;
+            double salario = 1275.33;
+    
+           System.out.println("Idade " + idade); 
+           System.out.println("Ano " + ano);   
+           System.out.println("cep " + cep); 
+           System.out.println("cpf " + cpf);   
+           System.out.println("pi " + pi);     
+           System.out.println("salario " + salario); 
+        
+    }
+}
